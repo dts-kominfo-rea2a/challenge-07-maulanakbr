a972bf371880d45c484d4bca451b7e9c2508098c
Maulana Akbar Yudistika <yudistika.akbar@gmail.com>
2022-06-11 10:12:49 +0700

    feat: challenge-07 done

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const content1 = await promiseTheaterIXX();
+    const content2 = await promiseTheaterVGC();
+    const result = content1.concat(content2);
+    return result.map((allTheater) => allTheater.hasil === emosi).reduce((a, b) => a + b, 0); 
+  }
+  catch (err) {
+    console.log(err);
+  };
+};
 
 module.exports = {
   promiseOutput,
